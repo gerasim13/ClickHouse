bc5e9a89ba8644d4e33b534e71e22ecd3179fdec
Павел Литвиненко <digitaldistortion@ya.ru>
2018-02-05 10:22:33 +0300

    no message

--- dbms/src/Dictionaries/MongoDBDictionarySource.cpp
+++ dbms/src/Dictionaries/MongoDBDictionarySource.cpp
@@ -263,8 +263,8 @@ BlockInputStreamPtr MongoDBDictionarySource::loadKeys(
                 case AttributeUnderlyingType::String:
                     if (attr.second.injective)
                     {
-                        Poco::MongoDB::ObjectId _id(get<String>((*key_columns[attr.first])[row_idx]));
-                        key.add(attr.second.name, Poco::MongoDB::ObjectId::Ptr(&_id));
+                        Poco::MongoDB::ObjectId _id((*key_columns[attr.first])[row_idx]);
+                        key.add(attr.second.name, _id);
                     }
                     else
                     {
